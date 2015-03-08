e66e7c76e163c5015894386f2db00f5d78e013b2
Greg Narajka <gregnar@gmail.com>
2015-03-08 16:24:17 -0600

    add validation for instagram

--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -1,17 +1,21 @@
 class Instagram < ActiveRecord::Base
   belongs_to :trip
   belongs_to :user, foreign_key: :user_name, primary_key: :insta_name
+  validates :time_posted, uniqueness: { scope: :user }
 
   def self.save_instagrams(json, trip_id)
-    binding.pry
     json['data'].each do |data|
-      create!(image_url: data['images']['standard_resolution']['url'] ,
-              caption: data['caption']['text'] ,
-              profile_pic: data['user']['profile_picture'],
-              user_name: data['user']['username'],
-              trip_id: trip_id,
-              time_posted:data['created_time'],
-             )
+      begin
+        create!(image_url: data['images']['standard_resolution']['url'] ,
+                caption: data['caption']['text'] ,
+                profile_pic: data['user']['profile_picture'],
+                user_name: data['user']['username'],
+                trip_id: trip_id,
+                time_posted:data['created_time'],
+               )
+      rescue ActiveRecord::RecordInvalid
+        next
+      end
     end
   end
 
